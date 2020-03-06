e3f8be523da0cb2aed17b3591b6e244883e89c11
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-03-06 10:00:04 -0800

    Shows piechart with default colors

--- chart.js
+++ chart.js
@@ -1,1 +1,17 @@
 // Your code here!
+var chart = c3.generate({
+  data: {
+      // iris data from R
+      columns: [
+        ['sleep', 8],
+        ['eat', 3],
+        ['code', 7],
+        ['relax', 2],
+        ['other', 4]
+      ],
+      type : 'pie',
+      onclick: function (d, i) { console.log("onclick", d, i); },
+      onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+      onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+  }
+});
