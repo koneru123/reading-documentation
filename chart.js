15d22e6299d0d176b9f4fe989d92c389495e8c92
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-03-06 10:17:10 -0800

    Change the piechart default colors to the colors specified

--- chart.js
+++ chart.js
@@ -10,6 +10,13 @@ var chart = c3.generate({
         ['other', 4]
       ],
       type : 'pie',
+      colors: {
+        sleep: '#D81159',
+        eat: '#8F2D56',
+        code: '#218380',
+        relax: '#FBB13C',
+        other: '#726DA8'
+      },
       onclick: function (d, i) { console.log("onclick", d, i); },
       onmouseover: function (d, i) { console.log("onmouseover", d, i); },
       onmouseout: function (d, i) { console.log("onmouseout", d, i); }
